591aad04a27dcd3099fdc5537ffeddaffda078dd
miksbuloan <136316957+miksbuloan@users.noreply.github.com>
2023-06-12 16:48:56 +0800

    Add files via upload

---
+++ checkout.php
@@ -0,0 +1,162 @@
+<?php
+
+@include 'config.php';
+
+if(isset($_POST['order_btn'])){
+
+   $name = $_POST['name'];
+   $number = $_POST['number'];
+   $email = $_POST['email'];
+   $method = $_POST['method'];
+   $flat = $_POST['flat'];
+   $street = $_POST['street'];
+   $city = $_POST['city'];
+   $state = $_POST['state'];
+   $country = $_POST['country'];
+   $pin_code = $_POST['pin_code'];
+
+   $cart_query = mysqli_query($conn, "SELECT * FROM `cart`");
+   $price_total = 0;
+   if(mysqli_num_rows($cart_query) > 0){
+      while($product_item = mysqli_fetch_assoc($cart_query)){
+         $product_name[] = $product_item['name'] .' ('. $product_item['quantity'] .') ';
+         $product_price = number_format($product_item['price'] * $product_item['quantity']);
+         $price_total += $product_price;
+      };
+   };
+
+   $total_product = implode(', ',$product_name);
+   $detail_query = mysqli_query($conn, "INSERT INTO `order`(name, number, email, method, flat, street, city, state, country, pin_code, total_products, total_price) VALUES('$name','$number','$email','$method','$flat','$street','$city','$state','$country','$pin_code','$total_product','$price_total')") or die('query failed');
+
+   if($cart_query && $detail_query){
+      echo "
+      <div class='order-message-container'>
+      <div class='message-container'>
+         <h3>thank you for shopping!</h3>
+         <div class='order-detail'>
+            <span>".$total_product."</span>
+            <span class='total'> total : $".$price_total."/-  </span>
+         </div>
+         <div class='customer-details'>
+            <p> Name : <span>".$name."</span> </p>
+            <p> Number : <span>".$number."</span> </p>
+            <p> Email : <span>".$email."</span> </p>
+            <p> Address : <span>".$flat.", ".$street.", ".$city.", ".$state.", ".$country." - ".$pin_code."</span> </p>
+            <p> Payment Method: <span>".$method."</span> </p>
+            <p>(*Pay when product arrives*)</p>
+         </div>
+            <a href='products.php' class='btn'>continue shopping</a>
+         </div>
+      </div>
+      ";
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>checkout</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<?php include 'header.php'; ?>
+
+<div class="container">
+
+<section class="checkout-form">
+
+   <h1 class="heading">complete your order</h1>
+
+   <form action="" method="post">
+
+   <div class="display-order">
+      <?php
+         $select_cart = mysqli_query($conn, "SELECT * FROM `cart`");
+         $total = 0;
+         $grand_total = 0;
+         if(mysqli_num_rows($select_cart) > 0){
+            while($fetch_cart = mysqli_fetch_assoc($select_cart)){
+            $total_price = number_format($fetch_cart['price'] * $fetch_cart['quantity']);
+            $grand_total = $total += $total_price;
+      ?>
+      <span><?= $fetch_cart['name']; ?>(<?= $fetch_cart['quantity']; ?>)</span>
+      <?php
+         }
+      }else{
+         echo "<div class='display-order'><span>your cart is empty!</span></div>";
+      }
+      ?>
+      <span class="grand-total"> grand total : $<?= $grand_total; ?>/- </span>
+   </div>
+
+      <div class="flex">
+         <div class="inputBox">
+            <span>Name</span>
+            <input type="text" placeholder="enter your name" name="name" required>
+         </div>
+         <div class="inputBox">
+            <span>Number</span>
+            <input type="number" placeholder="enter your number" name="number" required>
+         </div>
+         <div class="inputBox">
+            <span>Email</span>
+            <input type="email" placeholder="enter your email" name="email" required>
+         </div>
+         <div class="inputBox">
+            <span>Payment Method</span>
+            <select name="method">
+               <option value="cash on delivery" selected>Cash on Devlivery</option>
+               <option value="credit cart">Credit Cart</option>
+               <option value="paypal">Paypal</option>
+            </select>
+         </div>
+         <div class="inputBox">
+            <span>/Landmark/House Number</span>
+            <input type="text" placeholder="e.g. flat no." name="flat" required>
+         </div>
+         <div class="inputBox">
+            <span>Street</span>
+            <input type="text" placeholder="e.g. street name" name="street" required>
+         </div>
+         <div class="inputBox">
+            <span>Barangay</span>
+            <input type="text" placeholder="e.g. mumbai" name="city" required>
+         </div>
+         <div class="inputBox">
+            <span>Town</span>
+            <input type="text" placeholder="e.g. maharashtra" name="state" required>
+         </div>
+         <div class="inputBox">
+            <span>Province</span>
+            <input type="text" placeholder="e.g. india" name="country" required>
+         </div>
+         <div class="inputBox">
+            <span>pin code</span>
+            <input type="text" placeholder="e.g. 123456" name="pin_code" required>
+         </div>
+      </div>
+      <input type="submit" value="order now" name="order_btn" class="btn">
+   </form>
+
+</section>
+
+</div>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+   
+</body>
+</html>
